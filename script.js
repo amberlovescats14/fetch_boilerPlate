f6c3594da30d80f33231a6b029d913283c99a609
Amber Jones <amberlovescats14@gmail.com>
2019-08-16 22:14:04 -0500

    both ways

--- script.js
+++ script.js
@@ -2,22 +2,37 @@
 
 
 
-const getAPI = () => {
+// const getAPI = () => {
+//   let payload;
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res => res.json())
+//     .then(data => {
+//       payload = data
+//       showApi(payload)
+//     })
+// }
+// const showApi = (payload) => {
+//   console.log(`amber`)
+//   payload.map(user => console.log(user.name))
+// }
+// getAPI()
+
+
+function getAPI() {
   let payload;
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => res.json())
-    .then(data => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => res.json())
+      .then(data => {
       payload = data
-      showApi(payload)
+      showAPI(payload)
     })
+
 }
-const showApi = (payload) => {
-  console.log(`amber`)
+
+function showAPI(payload){
   payload.map(user => console.log(user.name))
 }
+
 getAPI()
-
-
-
 
 
